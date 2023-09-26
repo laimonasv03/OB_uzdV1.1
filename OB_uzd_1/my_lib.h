fc4c70034a3a21d5642e373fbcd06a4ca11ffc98
laimonasv03 <laimonasvailenka@gmail.com>
2023-09-27 00:00:28 +0300

    studentas

---
+++ OB_uzd_1/my_lib.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct studentas {
+	string vardas, pavarde;
+	vector <int> pazymiai;
+	int egz;
+	float rez;
+};
+
