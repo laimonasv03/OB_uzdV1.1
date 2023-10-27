b5aae73a2f8a1f5c358a7931df361006866505f0
laimonasv03 <laimonasvailenka@gmail.com>
2023-10-27 11:30:14 +0300

    bandau sulygiuot

--- OB_uzd_1/main.cpp
+++ OB_uzd_1/main.cpp
@@ -27,16 +27,16 @@ int main() {
 		cout << "1. vidurkiu\n2. mediana\n";
 		patikrink(pasirinkimas_balo);
 		if (pasirinkimas_balo == 1) {
-			cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(15) << "Galutinis(vid.)" << setw(20) << "AtmintiesAdresas\n";
+			cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(vid.)" << setw(20) << "AtmintiesAdresas\n";
 			for (auto& student : grupe) {
 				cout << left << setw(20) << student.pavarde << setw(20) << student.vardas << setw(20) << student.galutinis_vidurkis << &student << "\n";
 			}
 		}
 		else if (pasirinkimas_balo == 2) {
-			cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(15) << "Galutinis(med.)" << setw(20) << "AtmintiesAdresas\n";
+			cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(med.)" << setw(20) << "AtmintiesAdresas\n";
 			// studentai 
 			for (auto& student : grupe) {
-				cout << left << setw(20) << student.pavarde << setw(20) << student.vardas << setw(20) << student.galutinis_mediana << "\n";
+				cout << left << setw(20) << student.pavarde << setw(20) << student.vardas << setw(20) << student.galutinis_mediana << &student << "\n";
 			}
 		}
 	}
